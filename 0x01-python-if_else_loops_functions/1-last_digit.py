2a628915d670135bd5ba936664c6d9dad451722f
Fredrick Okara Ogunde <okaraogunde@gmail.com>
2023-06-07 21:53:03 +0000

    last digit correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ if number < 0:
     digit = -digit
 print("Last digit of {} is {} and is ".format(number, digit), end="")
 if digit > 5:
-    print("greater than 5\n")
+    print("greater than 5")
 elif digit == 0:
-    print("0\n")
+    print("0")
 else:
-    print("less than 6 and not 0\n")
+    print("less than 6 and not 0")
