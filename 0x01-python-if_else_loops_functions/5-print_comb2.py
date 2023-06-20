9d2743531df978113218e8d4dbbe4731c586139c
FREDRICK OKARA OGUNDE <okaraogunde@gmail.com>
2023-06-20 09:07:11 +0000

    print numbers 0 to 99 seperated by comma corrected

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 # print number 0 to 99 seperated by comma
 for num in range(0, 100):
     if num == 99:
-        print(f"{num}")
+        print("{}".format(num))
     else:
-        print(f"{num:02}", end=", ")
+        print("{:02}".format(num), end=", ")
