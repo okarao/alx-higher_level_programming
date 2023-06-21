8ff40a5465e1cee4767391be41f15ace4c9732fd
FREDRICK OKARA OGUNDE <okaraogunde@gmail.com>
2023-06-21 09:42:16 +0000

    magic calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    if a > b:
-        return a - b
-    elif a < b:
-        return a + b
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4,6):
+            c = add(c, i)
     else:
-        return a * b
+        return(sub(a, b))
