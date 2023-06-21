7fdea8bda37683cbfbc7e85a8c64d7273d329430
FREDRICK OKARA OGUNDE <okaraogunde@gmail.com>
2023-06-21 17:41:53 +0000

    find multiples of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     multiples = []
-    for n in range(len(may_list)):
+    for n in range(len(my_list)):
         if my_list[n] % 2 == 0:
             multiples.append(True)
         else:
