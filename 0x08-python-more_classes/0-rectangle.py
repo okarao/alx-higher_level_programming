8c974559f80a6a79b4a8754ba6446b5f46fc1b1c
FREDRICK OKARA OGUNDE <okaraogunde@gmail.com>
2023-07-30 13:11:30 +0000

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+'''Define an empty class called Rectangle that defines a rectangle'''
+
+
+class Rectangle:
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+###ALX SOFTWARE ENGINEERING PROGRAM##
+***High Level Programing in Python***
+Object Oriented Programing: Classes
