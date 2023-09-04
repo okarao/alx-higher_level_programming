33d77d23f129713ff406cd5991883c5c5e8f3a07
FREDRICK OKARA <okaraogunde@gmail.com>
2023-09-04 14:07:39 +0000

    Delete folders

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Define an empty class called Rectangle that defines a rectangle"""
-
-
-class Rectangle:
-    """ Respresent a rectangle"""
-    pass
--- 0x08-python-more_classes/README.md
+++
@@ -1,3 +0,0 @@
-###ALX SOFTWARE ENGINEERING PROGRAM##
-***High Level Programing in Python***
-Object Oriented Programing: Classes
