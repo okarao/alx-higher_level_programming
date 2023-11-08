441596544df0c2028582b81cb07fb88b98485078
FREDRICK OKARA <okaraogunde@gmail.com>
2023-11-08 03:19:41 +0000

    corrections on read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""Module containing the function read file"""
+
 
 def read_file(filename=""):
     """
     Read a text file (UTF8) and print its content to stdout.
 
-    :param filename: The name of the file to read (default is an empty string).
+    Args:
+        filename(str, optional): The name of the file to read (default is an empty string).
     """
-    with open(filename, encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
-        print(content)
+        print(content, end='')
