e30381ff276a9bf061a48220898406ba9a089c97
FREDRICK OKARA <okaraogunde@gmail.com>
2023-11-08 03:28:39 +0000

    Write to a file corrections

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""Module containing the function write_file"""
+
+
 
 def write_file(filename="", text=""):
     """
     Write a string to a text file (UTF8) and return the number of characters written.
 
-    :param filename: The name of the file to write to (default is an empty string).
-    :param text: The string to write to the file (default is an empty string).
-    :return: The number of characters written.
+    Args:
+        filename(str, optional): The name of the file to write to (default is an empty string).
+        text(str, optional): The string to write to the file (default is an empty string).
+    Returns:
+        int: number of characters written.
     """
-    with open(filename, mode='w', encoding='utf-8') as file:
+    with open(filename,'w', encoding='utf-8') as file:
         chars_written = file.write(text)
     return chars_written
