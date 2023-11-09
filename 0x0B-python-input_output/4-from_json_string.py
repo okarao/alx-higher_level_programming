9be771fe04044c29c3bd331e09b522d613924737
FREDRICK OKARA <okaraogunde@gmail.com>
2023-11-09 02:43:58 +0000

    From JSON string to Object corrected

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """"Module containing  a function that returns an object (Python data structure) represented by a JSON string"""
 
+
 import json
+
 
 def from_json_string(my_str):
     """
