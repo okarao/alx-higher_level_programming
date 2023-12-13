1dcd9a003eed1f7ebcdc6e7366872c0f38aa7d9f
FREDRICK OKARA <okaraogunde@gmail.com>
2023-12-13 22:58:29 +0000

    16. Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- List all records where name is not null
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '';
 
