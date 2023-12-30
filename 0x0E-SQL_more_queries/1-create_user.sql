ce3788ef9afaffea19d1bf5cb2947279e34c407a
FREDRICK OKARA <okaraogunde@gmail.com>
2023-12-30 21:17:55 +0000

    1. Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,4 +4,4 @@
 -- If the user user_0d_1 already exists, your script should not fail
 
 CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'sser_0d_1'@'localhost' WITH GRANT OPTION;
