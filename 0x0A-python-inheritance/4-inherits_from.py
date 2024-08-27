afe5beedadbda4d2912deb62d7db63146664a8ab
FREDRICK OKARA <okaraogunde@gmail.com>
2024-08-27 16:45:15 +0000

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Defines a function that returns true if the object is an instance of a class
+that inherited from the specified class
+'''
+
+def inherits_from(obj, a_class):
+    """
+    Determines if the object is an instance of a class that
+    inherited from the specified class
+
+    Args:
+        obj: the object
+        a_class: the specified class
+    Returns:
+        True or False
+    """
+    return (isinstance(obj, a_class) and type(obj) is not a_class)
