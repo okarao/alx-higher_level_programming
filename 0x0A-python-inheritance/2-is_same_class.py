4bfe3688d513a1f5d3b167f206debc4d286e4958
FREDRICK OKARA <okaraogunde@gmail.com>
2024-08-27 15:25:46 +0000

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-'''Defines a function that returns true 
+'''Defines a function that returns true
 if the object is exactly an instance of the specified class
 '''
+
 
 def is_same_class(obj, a_class):
     """
@@ -10,8 +11,8 @@ def is_same_class(obj, a_class):
     Args:
         obj: object
         a_class: the class
-    
+
     Returns:
         True if object is exactly instance of class or false
     """
-    return (type(obj) == a_class)
+    return (isinstance(obj, a_class))
