8ceebcfe63a7aea55974fedd7d92627ab4d2d1df
FREDRICK OKARA <okaraogunde@gmail.com>
2024-08-28 08:15:52 +0000

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -2,3 +2,17 @@
 '''
 Defines a class BaseGeometry based on 6-base_geometry
 '''
+
+class BaseGeometry:
+    """
+    Class BaseGeometry based on 6-basegeometry
+    """
+
+    def area(self):
+        """
+        public instance class area
+
+        Raises:
+            Exception
+
+        ""
