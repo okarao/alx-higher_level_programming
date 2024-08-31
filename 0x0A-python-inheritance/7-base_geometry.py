fdfe3a6d7b33334452b9c94b6f86cb47be46db16
FREDRICK OKARA <okaraogunde@gmail.com>
2024-08-31 07:52:13 +0000

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,3 +15,12 @@ class BaseGeometry:
         public instance method 
         """
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates that a value is a positive integer.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
